281546a591af5dbbae0867bd3a64f232282c8779
root <mckinneyschool456@gmail.com>
2022-11-22 01:22:33 +0000

    updated project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 RSpec.describe Project, type: :model do
   context "validations tests" do
-    it "ensures the description is present" do
+    it "ensures the title is present" do
       project = Project.new(description: "Content of the description")
+      expect(project.valid?).to eq(false)
+    end
+
+    it "ensures the description is present" do
+      project = Project.new(title: "Content of the title")
       expect(project.valid?).to eq(false)
     end
     
@@ -12,6 +17,19 @@ RSpec.describe Project, type: :model do
 
     end
   end
+
+
   context "scopes tests" do
+    let(:params) { { title: "Title", description: "some description" } }
+    before(:each) do
+          Project.create(params)
+          Project.create(params)
+          Project.create(params)
+    end
+
+    it "should return all projects" do
+      expect(Project.count).to eq(3)
+    end
+
   end
 end
