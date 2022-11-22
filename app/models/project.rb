a35f7b2d6384db89c84c280e89dc03be2fce6571
root <mckinneyschool456@gmail.com>
2022-11-22 01:54:03 +0000

    Pushing for CircleCI

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+    #require title and description
+    validates_presence_of :title, :description
 end
