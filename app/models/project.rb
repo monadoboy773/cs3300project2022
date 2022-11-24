ea5900371349a94befa20296825f3ec3ac401cac
root <mckinneyschool456@gmail.com>
2022-11-24 00:21:04 +0000

    Iteration 4 finished

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
     #require title and description
-    validates_presence_of :title, :description
+    validates :title, presence: true
+    validates :description, presence: true
+
+    #validates_presence_of :title, :description
 end
